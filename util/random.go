f69aeba28b9444d6937398e4d517f9001fa74953
Sanyam <shanujindal48@gmail.com>
2022-06-26 22:56:32 +0530

    added random generator; tests for accounts complete

---
+++ util/random.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghujklmnopqrstuvwxyz"
+
+// This init function is called everytime the package is first used.
+func init()  {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Generate a random int between min and max
+func RandomInt(min, max int64) int64{
+	return min + rand.Int63n(max-min+1)
+}
+
+// Generate a random string of n characters
+func RandomString(n int) string{
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i:=0; i < n; i++{
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// Get owner name of length 6.
+func RandomOwner() string{
+	return RandomString(6)
+}
+
+func RandomMoney() int64{
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency()string{
+	currencies := []string{"USD", "EUR", "CAD"}
+
+	return currencies[rand.Intn(3)]
+}
