1427b52019f53c042c76073102c688987ed9e92c
Sanyam <shanujindal48@gmail.com>
2022-07-10 16:22:54 +0530

    fix migrate down file 2

--- db/migration/000002_add_users.down.sql
+++ db/migration/000002_add_users.down.sql
@@ -1,1 +1,5 @@
-DROP TABLE "users";
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "owner_currency_key";
+
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "account_owner_fkey";
+
+DROP TABLE IF EXISTS "users";
