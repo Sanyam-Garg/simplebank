bd6671155e6e4c49099e3a69d6ea633ea65f4225
Sanyam <shanujindal48@gmail.com>
2022-07-10 16:51:43 +0530

    add tests for users

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -9,21 +10,21 @@ import (
 const alphabet = "abcdefghujklmnopqrstuvwxyz"
 
 // This init function is called everytime the package is first used.
-func init()  {
+func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 // Generate a random int between min and max
-func RandomInt(min, max int64) int64{
+func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
 // Generate a random string of n characters
-func RandomString(n int) string{
+func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
 
-	for i:=0; i < n; i++{
+	for i := 0; i < n; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
@@ -32,16 +33,20 @@ func RandomString(n int) string{
 }
 
 // Get owner name of length 6.
-func RandomOwner() string{
+func RandomOwner() string {
 	return RandomString(6)
 }
 
-func RandomMoney() int64{
+func RandomMoney() int64 {
 	return RandomInt(0, 1000)
 }
 
-func RandomCurrency()string{
+func RandomCurrency() string {
 	currencies := []string{USD, EUR, CAD}
 
 	return currencies[rand.Intn(len(currencies))]
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
