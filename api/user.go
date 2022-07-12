bb66e4c3dfa7af273c134683d43aeffd9d749f8e
Sanyam <shanujindal48@gmail.com>
2022-07-12 12:48:08 +0530

    create user api

---
+++ api/user.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	db "github.com/Sanyam-Garg/simplebankgo/db/sqlc"
+	"github.com/Sanyam-Garg/simplebankgo/util"
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username" binding:"required,alphanum"`
+	FullName          string    `json:"full_name" binding:"required"`
+	Email             string    `json:"email" binding:"required,email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	args := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, args)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errResponse(pqErr))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	rsp := createUserResponse{
+		Username: user.Username,
+		FullName: user.FullName,
+		Email:    user.Email,
+		CreatedAt: user.CreatedAt,
+		PasswordChangedAt: user.PasswordChangedAt,
+	}
+
+	ctx.JSON(http.StatusCreated, rsp)
+}
